8bb65d8fca9e9d9146400058980abfb936542557
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2026-04-29 08:09:49 +0530

    Add project files.

---
+++ ConsoleGitProjectForJenkins/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGitProjectForJenkins
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a = 10, b = 20, c;
+            c = a + b;
+            Console.WriteLine("Add="+c);
+        }
+        static void Main(string[] args)
+        {
+            Program p =new Program();
+            p.Add();
+        }
+    }
+}
